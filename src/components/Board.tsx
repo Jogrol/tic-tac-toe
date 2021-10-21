847b0480fb00015cc2b25807006cfea088d9b3e1
Joey Grolleman <joeygrolleman@gmail.com>
2021-10-21 19:29:05 +0200

    Add handleGame turn logic and update tailwind with hover animation state

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,18 +3,28 @@ import { PlayerEnum } from "../state/gameReducer";
 
 interface BoardPropsModel {
   squares: (PlayerEnum | number)[];
-
+  handleGameTurn: Function;
 }
 
-const Board = ({ squares }: BoardPropsModel) => {
+const Board = ({ squares, handleGameTurn }: BoardPropsModel) => {
+  const isClicked = (square: PlayerEnum | number) => {
+    return square === PlayerEnum.Player_1 || square === PlayerEnum.Player_2;
+  };
 
   return (
-    <div className="grid gap-4 grid-cols-3 p-4">
-      {squares.map((i) => {
+    <div className='grid gap-4 grid-cols-3 p-4 '>
+      {squares.map((i, index) => {
         return (
-          <div key={i} className='flex items-center justify-center rounded-lg border-2 py-6'>
-            <span className="text-7xl">{i}</span>
-          </div>
+          <button
+            onClick={() => handleGameTurn(i)}
+            key={index}
+            className={`flex items-center justify-center rounded-lg border-2 py-6 ${
+              isClicked(i)
+                ? `cursor-default`
+                : "cursor-pointer hover:animate-pulse"
+            }`}>
+            <span className='text-7xl'>{i}</span>
+          </button>
         );
       })}
     </div>
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,19 +1,41 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { gameActions } from "../state/gameActions";
 import { PlayerEnum } from "../state/gameReducer";
 import { gameSelectors } from "../state/gameSelector";
 
 export interface UseGameModel {
   boardSquares: any[];
   currentPlayer: PlayerEnum;
+  handleGameTurn: Function;
 }
 
 const useGame = (): UseGameModel => {
+  const dispatch = useDispatch();
   const boardSquares = useSelector(gameSelectors.SelectBoardSquares);
   const currentPlayer = useSelector(gameSelectors.SelectCurrentPlayer);
 
+  const handleGameTurn = (index: number): void => {
+    let updatedBoardSquares = [...boardSquares];
+    updatedBoardSquares[index] = currentPlayer;
+
+    togglePlayer();
+    dispatch(gameActions.setBoardSquares(updatedBoardSquares));
+  };
+
+  const togglePlayer = (): void => {
+    dispatch(
+      gameActions.setPlayer(
+        currentPlayer === PlayerEnum.Player_1
+          ? PlayerEnum.Player_2
+          : PlayerEnum.Player_1
+      )
+    );
+  };
+
   return {
     boardSquares,
-    currentPlayer
+    currentPlayer,
+    handleGameTurn,
   };
 };
 
